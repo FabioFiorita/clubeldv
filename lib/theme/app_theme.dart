56a52ea9739388957d66add1ba08700ea515850d
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-22 13:59:54 -0300

    feat: updating app theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,32 +2,8 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-ThemeData appTheme = FlexThemeData.light(
-  scheme: FlexScheme.redM3,
-  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-  blendLevel: 7,
-  subThemesData: const FlexSubThemesData(
-    blendOnLevel: 10,
-    blendOnColors: false,
-    useM2StyleDividerInM3: true,
-  ),
-  visualDensity: FlexColorScheme.comfortablePlatformDensity,
-  useMaterial3: true,
-  swapLegacyOnMaterial3: true,
-  textTheme: textTheme,
-);
-
-ThemeData darkAppTheme = FlexThemeData.dark(
-  scheme: FlexScheme.redM3,
-  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-  blendLevel: 13,
-  subThemesData: const FlexSubThemesData(
-    blendOnLevel: 20,
-    useM2StyleDividerInM3: true,
-  ),
-  visualDensity: FlexColorScheme.comfortablePlatformDensity,
-  useMaterial3: true,
-  swapLegacyOnMaterial3: true,
+ThemeData appTheme = ThemeData(
+  primarySwatch: Colors.red,
   textTheme: textTheme,
 );
 
